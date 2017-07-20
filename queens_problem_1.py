b8b9057098dd6db55f3319117cb352caa159caae
xiaolun.cao <xiaolun.cao@gmail.com>
2017-07-20 22:56:46 +0800

    funcional update

--- queens_problem_1.py
+++ queens_problem_1.py
@@ -4,12 +4,12 @@ import random
 
 def conflict(state, nextX):
   nextY=len(state)
-  for i in range(nextY)
+  for i in range(nextY):
     if abs(state[i]-nextX) in (0, nextY-i):
       return True
       
-def queens(num=4, state=());
-  print 'Call queens, ', 'num: ', num, 'state: ', state
+def queens(num=4, state=()):
+  #print 'Call queens, ', 'num: ', num, 'state: ', state
   for pos in range(num):
     if not conflict(state, pos):
       if len(state) == num-1:
@@ -17,14 +17,20 @@ def queens(num=4, state=());
       else:
         for result in queens(num, state+(pos,)):
           yield (pos,)+result
-    else:
-      print 'Confliction violated! pos: ', pos, 'state: ', state
+    #else:
+      #print 'Confliction violated! pos: ', pos, 'state: ', state
       
 def prettyprint(solution):
   def line(pos, length=len(solution)):
     return ' . '*(pos)+' X '+' . '*(length-pos-1)
   for pos in solution:
     print line(pos)
-    
-print queens(8)
-prettyprint(random.choice(list(queens(8))))
+
+solution_list = list(queens(4))
+print solution_list
+#prettyprint(random.choice(list(queens(4))))
+for i in range(len(solution_list)):
+  print 'Solution No. i'
+  prettyprint(solution_list[i])
+  print ''
+  
