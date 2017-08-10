2e6a9b4bd70eb1545361792de839a5e483468b91
Xiaolun Cao <xiaolun.cao@gmail.com>
2017-08-10 21:22:45 +0800

    Update josephus_problem2.cpp

--- josephus_problem2.cpp
+++ josephus_problem2.cpp
@@ -37,7 +37,17 @@ public int getLive(int i, int m){
 
 /*
 Now analyze the time complexity
+The idea is to generate a new list after killing every element and iterate the new list, for example, after the first killing:
 
+old: m+1, m+2, m+3, ... , n, 1, 2, 3, ..., m-1
+new: 1, 2, 3, ..., n-m, n-m+1, n-m+2, ..., n-1
+
+Above shows the new mapping relation after kill the mth item. Now interpret the mapping function easily as
+for ever old, exists old = (new + m) % n, Note that the solution is not unique
+So the recursive function is: f(n) = (f(n-1) + m) % n
+Appling the master method: a == 1, b == (n-1)/n, f(n) = O(1)
+So time complexity is T(n) = 1 * T(n * (n-1) / n) + O(1) = T(n-1)+O(1) <= O(n)
+Thus appling the recursive way, the time complexity of josephus kill problem is optimized to O(n)
 */
 
 
