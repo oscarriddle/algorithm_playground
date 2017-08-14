f7ad48dafab43cff629c648098d58a3c146438cb
Xiaolun Cao <xiaolun.cao@gmail.com>
2017-08-14 14:54:22 +0800

    Create binary_tree_morris_pre_traverse.cpp

---
+++ binary_tree_morris_pre_traverse.cpp
@@ -0,0 +1,34 @@
+public class Node {
+  public int value;
+  public left;
+  public right;
+  public Node (int data) {
+    this.value = data;
+  }
+}
+
+public void morrisPre(Node head) {
+  if (head == null) {return;}
+  Node cur1 = head;
+  Node cur2 = null;
+  while (cur1 != null) {
+    cur2 = cur1.left;
+    if (cur2 != null) {
+      while (cur2.right != null && cur2.right != cur1) {
+        cur2 = cur2.right;
+      }
+      if(cur2.right == null) {
+        cur2.right =cur1;
+        System.out.print(cur1.value+"");
+        cur1=cur1.left;
+        continue;
+      } else {
+        cur2.right = null;
+      }
+    } else {
+      System.out.print(cur1.value+"");
+    }
+    cur1 = cur1.right;
+  }
+  System.out.println();
+}
