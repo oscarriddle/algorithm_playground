820aecf0586a9f275a0db97d5acc92952b02bda2
Xiaolun Cao <xiaolun.cao@gmail.com>
2017-08-14 14:32:43 +0800

    Update binary_tree_morris_traverse.cpp
    update Mid-order printing traverse

--- binary_tree_morris_traverse.cpp
+++ binary_tree_morris_traverse.cpp
@@ -1,1 +1,38 @@
+// Node Declaration
+public class Node {
+  public int value;
+  public Node left;
+  public Node right;
+  public Node (int data) {
+    this.value = data;
+  }
+} 
 
+
+//Mid-order Printing Traverse
+
+public void morrisMid {
+  if (head == null) {
+   return; 
+  }
+  Node cur1 = head;
+  Node cur2 = null;
+  while (cur1 != null){
+    cur2 = cur1.left;
+    if (cur2 != null) {
+     while(cur2 != null && cur2.right != cur1) {
+      cur2 = cur2.right;
+     }
+     if (cur2.right == null) {
+       cur2.right = cur1;
+       cur1 = cur1.left;
+       continue;
+     }else{
+       cur2.right = null;
+     }
+    }
+    System.out.print(cur1.value+"");
+    cur1=cur1.right;
+  }
+  System.out.println();
+}
